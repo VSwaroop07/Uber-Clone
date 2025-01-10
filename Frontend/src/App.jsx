79ef6d24c605498784fc74c08d718cdc3ebde098
Swaroop Vyawahare <Vyawahareswarup19@gmail.com>
2025-01-10 23:01:53 +0530

    Fix in Tailwind Css

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import {Routes, Route} from './Routes';
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/home';
+import UserLogin from './pages/userLogin';
+import UserRegister from './pages/userRegister';
+import CaptainLogin from './pages/captainLogin';
+import CaptionRegister from './pages/captainRegister';
+
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={home}  />
+      <Route path='/' element={<Home />}  />
+      <Route path='/login' element={<UserLogin />}  />
+      <Route path='/signup' element={<UserRegister />}  />
+      <Route path='/captain-login' element={<CaptainLogin />}  />
+      <Route path='/captain-signup' element={<CaptionRegister />}  />
     </Routes>
   )
 }
--- Frontend/src/pages/home.jsx
+++ Frontend/src/pages/home.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const home = () => {
   return (
-    <div>home</div>
+    <div>
+        <div className="h-screen flex justify-between flex-col w-full bg-red-400">
+            <div className="bg-white">
+            <h2 className="">Get Started With Uber.</h2>
+            <button>Continue</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
